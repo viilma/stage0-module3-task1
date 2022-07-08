9181d799d5080dbd61e3dd429aec139b0089f1a5
Vilma Zaveckienė <v.zaveckiene@gmail.com>
2022-07-08 23:03:23 +0300

    All Apps fixed

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println( "I'm"+"\n"+"updated"+"\n"+"can"+"\n"+"be"+"\n"+"compiled."+"\n"+"And"+"\n"+"can"+"\n"
-                +"write"+"\n"+"to"+"\n"+"console");
+        System.out.println( "I'm updated" + "\n" + "can be compiled. And can write to console");
     }
 }
