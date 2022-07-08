012e210737295a3686486c4a4266261d596e0c0b
Vilma Zaveckienė <v.zaveckiene@gmail.com>
2022-07-08 22:00:45 +0300

    FillGapsApp fixed

--- src/main/java/lang/print/gaps/task1/FillGapsApp.java
+++ src/main/java/lang/print/gaps/task1/FillGapsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class FillGapsApp {
     public static void main(String[] args) {
-        System.out.println("\n"+"Fixed"+"\n");
+        System.out.println("Fixed"+"\n");
     }
 }
